707ecb4cbeb75ff8508fd8a7c53dc35587bf615b
Karifaniiia <lustina05@bk.ru>
2024-09-06 18:40:55 +0300

    Update Cat.java

--- Taskk1/Cat.java
+++ Taskk1/Cat.java
@@ -3,8 +3,8 @@ package com.example.demo;
 public class Cat extends Animal {
     private static final int catMaxSwimDist = 0;
     public static int count = 0;
-    public Cat(String name, int age,  int maxRunDist){
-        super(name,age,maxRunDist, catMaxSwimDist);
+    public Cat(String name, int maxRunDist){
+        super(name,maxRunDist, catMaxSwimDist);
         count++;
     }
 }
