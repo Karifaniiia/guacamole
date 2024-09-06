69d718bb018b3f8098bcdb30180aeeaf49929bfc
Karifaniiia <lustina05@bk.ru>
2024-09-06 18:42:08 +0300

    Update Turtle.java

--- Taskk1/Turtle.java
+++ Taskk1/Turtle.java
@@ -2,9 +2,8 @@ package com.example.demo;
 
 public class Turtle extends Reptile{
 public static int count = 0;
-public static final int years = 200;
-public Turtle(String name, int age,int maxRunDist,int maxSwimDist){
-    super(name,age,maxRunDist,maxSwimDist,years);
+public Turtle(String name, int maxRunDist,int maxSwimDist,int age){
+    super(name,maxRunDist,maxSwimDist,age);
     count++;
 }
 }
