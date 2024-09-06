45b1d0fa25745ddd6fbcf8fc0e2ee2a308d26dcd
Karifaniiia <lustina05@bk.ru>
2024-09-06 18:42:29 +0300

    Update Lizard.java

--- Taskk1/Lizard.java
+++ Taskk1/Lizard.java
@@ -2,9 +2,8 @@ package com.example.demo;
 
 public class Lizard extends Reptile{
    public static int count = 0;
-   public static final int years = 10;
-   public Lizard(String name, int age, int maxRunDist, int maxSwimRun){
-       super(name,age,maxRunDist,maxSwimRun,years);
+   public Lizard(String name, int maxRunDist, int maxSwimRun,int age){
+       super(name,maxRunDist,maxSwimRun,age);
        count++;
    }
 
