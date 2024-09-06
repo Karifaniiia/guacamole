d2bf44ad5052cb465338bc490eb425b0cae9165e
Karifaniiia <lustina05@bk.ru>
2024-09-06 18:41:11 +0300

    Update Dog.java

--- Taskk1/Dog.java
+++ Taskk1/Dog.java
@@ -2,8 +2,8 @@ package com.example.demo;
 
 public class Dog extends Animal{
     public static int count = 0;
-    public Dog (String name, int age, int maxRunDist, int maxSwimDist){
-        super(name,age,maxRunDist,maxSwimDist);
+    public Dog (String name,  int maxRunDist, int maxSwimDist){
+        super(name,maxRunDist,maxSwimDist);
         count++;
     }
 }
