52e97e152a7fda4d3598a8d1fd11559b7095ad44
Karifaniiia <lustina05@bk.ru>
2024-09-06 18:42:44 +0300

    Update Crocodile.java

--- Taskk1/Crocodile.java
+++ Taskk1/Crocodile.java
@@ -2,9 +2,8 @@ package com.example.demo;
 
 public class Crocodile extends Reptile{
     public static int count = 0;
-    public static final int years = 200;
-    public Crocodile(String name, int age, int maxRunDist, int maxSwimDist) {
-        super(name, age, maxRunDist, maxSwimDist, years);
+    public Crocodile(String name, int maxRunDist, int maxSwimDist,int age) {
+        super(name, maxRunDist, maxSwimDist,age);
         count++;
     }
 }
