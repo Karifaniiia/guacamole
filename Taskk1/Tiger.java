6908256ea11935e32a89e2c8d35b9f9b111255fe
Karifaniiia <lustina05@bk.ru>
2024-09-06 18:41:30 +0300

    Update Tiger.java

--- Taskk1/Tiger.java
+++ Taskk1/Tiger.java
@@ -2,8 +2,8 @@ package com.example.demo;
 
 public class Tiger extends Animal {
     public static int count = 0;
-    public Tiger(String name,int age,int maxRunDist, int maxSwimDist){
-        super(name,age,maxRunDist, maxSwimDist);
+    public Tiger(String name,int maxRunDist, int maxSwimDist){
+        super(name,maxRunDist, maxSwimDist);
         count++;
     }
 }
