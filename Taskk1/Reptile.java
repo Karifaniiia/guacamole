93496fb275182e7d63ad641c7a35da83da5232b8
Karifaniiia <lustina05@bk.ru>
2024-09-06 18:41:52 +0300

    Update Reptile.java

--- Taskk1/Reptile.java
+++ Taskk1/Reptile.java
@@ -2,15 +2,15 @@ package com.example.demo;
 
 public abstract class Reptile extends Animal{
     public static int count = 0;
-    public String name;
-    public int years;
-    public Reptile(String name,int age,int maxRunDist, int maxSwimDist, int years){
-        super(name,age,maxRunDist,maxSwimDist);
-        this.name = name;
-        this.years = years;
+    private int age;
+
+    public Reptile(String name, int maxRunDist, int maxSwimDist, int age) {
+        super(name, maxRunDist, maxSwimDist);
+        this.age = age;
         count++;
     }
-public void lifespan(){
-        System.out.println("Продолжительность жизни " + this.name + " - " + this.years + " лет.");
+
+    public void lifespan(){
+        System.out.println("Продолжительность жизни " + this.name + " - " + this.age + " лет.");
 }
 }
